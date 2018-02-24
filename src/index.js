b05c030f0f5e4e88a746895d78594a2733815522
konserv32 <konserv32@gmail.com>
2018-02-24 22:26:52 +0300

    i'm confused

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+var answer=0;
+// разложим основание на простые множители
+var numOfMax=1;
+var max=2;
+var num=base;
+while (num/max!==1) {
+   if (num%max===0) {
+       num=num/max;
+       numOfMax++;
+   } else {
+       max++;
+       numOfMax=1;
+   }
+  
+}
+
+
+
+for (var i=max;i<=number;i=i+max) {
+    for (var x=max; i%x===0; x=x*max) {
+        answer++;
+    }   
+}
+
+answer=Math.floor(answer/numOfMax);
+return(answer);
 }
